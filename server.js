a79386901a7b40115b825cab7713d3c966c9987f
manjitsagoo <manjit.sagoo@gmail.com>
2017-10-03 10:40:37 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express();
 const winston = require('winston');
 const dateFormat = require('dateformat');
 const version = require('./package.json');
 
+const app = express();
 app.use(morgan('combined'));
 winston.level = process.env.LOG_LEVEL;
 
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({limit: '5mb',  extended: true }));
 app.use(bodyParser.json({limit: '5mb'}));
 var router = express.Router();
 
-//save clients in database
+//POST 
 router.post('/test', function (req, res) {
     var data = JSON.stringify(req.body);
     var date = Date.now();
@@ -30,20 +30,22 @@ router.post('/test', function (req, res) {
      var obj = new Object(); 
      obj.date = Date.now();
      obj.message = message;
-     winston.log("Receiced " + message);
+     winston.log('info',,"Receiced " + message);
      res.send(JSON.stringify(obj));
   })
 
+//GET
 router.get('/version', function (req,res) {
     var obj = new Object(); 
     obj.Version = version.version;
     obj.Env = process.env;
+    winston.log('info',JSON.stringify(obj));
     res.send(JSON.parse(JSON.stringify(obj)));
 });
    
-
+//API
 app.use('/api', router);
 var port = process.env.PORT||3000;
 var server = app.listen(port, function(){
-	winston.log("info",'/api listening on ' + port);
+    console.log('/api listening on port' + server.address().port);
 });
