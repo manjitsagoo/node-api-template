1c886c9034d0f5e2eaeda14199b63f7edfc29c50
manjitsagoo <manjit.sagoo@gmail.com>
2017-10-03 08:20:23 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const winston = require('winston');
 const dateFormat = require('dateformat');
 const version = require('./package.json');
 
+app.use(morgan('combined'));
 winston.level = process.env.LOG_LEVEL;
 
 app.use(cors({
@@ -21,21 +23,21 @@ app.use(bodyParser.json({limit: '5mb'}));
 var router = express.Router();
 
 //save clients in database
-app.post('/test', function (req, res) {
+router.post('/test', function (req, res) {
     var data = JSON.stringify(req.body);
+    var date = Date.now();
     var message = "Received:" + data + " at " + dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-    console.log(message);
      var obj = new Object(); 
      obj.date = Date.now();
      obj.message = message;
-    res.send(JSON.stringify(obj));
+     winston.log("Receiced " + message);
+     res.send(JSON.stringify(obj));
   })
 
-
 router.get('/version', function (req,res) {
-    winston.log("info","GET /version");
     var obj = new Object(); 
     obj.Version = version.version;
+    obj.Env = process.env;
     res.send(JSON.parse(JSON.stringify(obj)));
 });
    
