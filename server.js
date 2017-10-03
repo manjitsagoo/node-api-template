621641b0733c4183ecf2b3e55c1bb7aaa01a7097
manjitsagoo <manjit.sagoo@gmail.com>
2017-10-03 10:54:36 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -16,35 +16,43 @@ app.use(cors({
     'origin': '*',
     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
     'preflightContinue': false
-   }));
+}));
 
-app.use(bodyParser.urlencoded({limit: '5mb',  extended: true }));
-app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+app.use(bodyParser.json({ limit: '5mb' }));
 var router = express.Router();
 
-//POST 
+//POST api/test
 router.post('/test', function (req, res) {
     var data = JSON.stringify(req.body);
     var date = Date.now();
     var message = "Received:" + data + " at " + dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-     var obj = new Object(); 
-     obj.date = Date.now();
-     obj.message = message;
-     winston.log('info',"Received " + message);
-     res.send(JSON.stringify(obj));
-  })
+    var obj = new Object();
+    obj.date = Date.now();
+    obj.message = message;
+    winston.log('info', "Received " + message);
+    res.send(JSON.stringify(obj));
+})
 
-//GET
-router.get('/version', function (req,res) {
-    var obj = new Object(); 
+//GET api/hello/name
+router.get('/hello/:name', function (req, res) {
+    var obj = new Object();
+    obj.Hello = req.params.name;
+    winston.log('info', JSON.stringify(obj));
+    res.send(JSON.parse(JSON.stringify(obj)));    
+});
+
+//GET api/version
+router.get('/version', function (req, res) {
+    var obj = new Object();
     obj.Version = version.version;
-    winston.log('info',JSON.stringify(obj));
+    winston.log('info', JSON.stringify(obj));
     res.send(JSON.parse(JSON.stringify(obj)));
 });
-   
+
 //API
 app.use('/api', router);
-var port = process.env.PORT||3000;
-var server = app.listen(port, function(){
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
     console.log('/api listening on port: ' + server.address().port);
 });
